df68f7bb7892a4e6566d4f82851ab662cb0ebc81
Aneesh Vempaty <aneeshvempaty@Aneesh-MBP.attlocal.net>
2020-06-11 13:21:44 -0700

    Completed till 2.3 of Chapter 2

--- chapter1.js
+++ chapter1.js
@@ -4,28 +4,27 @@
 //Implement an algorithm to determine if a string has all unique characters.
 //What if you cannot use additional data structures?
 
-//Approach 1: Using map(), O(n) time + space, using for loop as String 
+//Approach 1: Using map(), O(n) time + space, using for loop as String
 //would need to be converted to array using split() to use array methods
-var isUnique = function(string) {
+var isUnique = function (string) {
     let map = new Map()
-    for(let i in string) { //i gives index
-        if(map.has(string.charAt(i))) {
+    for (let i in string) {
+        //i gives index
+        if (map.has(string.charAt(i))) {
             return false
-        }
-        else {
+        } else {
             map.set(string.charAt(i), 1)
         }
     }
     return true
 }
 
-
 //Approach 2: Using lastIndexOf(), O(n^2) time, O(1) space
 //Can use other string methods to solve this, but approach
-//would require O(n^2) time 
-isUnique = function(string) {
-    for(let i in string) {
-        if(string.lastIndexOf(string.charAt(i)) != i) {
+//would require O(n^2) time
+isUnique = function (string) {
+    for (let i in string) {
+        if (string.lastIndexOf(string.charAt(i)) != i) {
             return false
         }
     }
@@ -36,49 +35,47 @@ isUnique = function(string) {
 console.log(isUnique('abc'))
 console.log(isUnique('dbcd'))*/
 
-
 //1.2 Check Permutation
-//Given two strings, write a method to decide if one is a 
+//Given two strings, write a method to decide if one is a
 //permutation of the other.
 
 //Approach 1: Using Map() O(n) time, O(n) space
 
-var isPermutation = function(s1, s2) {
-    if(s1.length !== s2.length) return false
+var isPermutation = function (s1, s2) {
+    if (s1.length !== s2.length) return false
     map = new Map()
-    for(let i in s1) {
-        if(map.has(s1.charAt(i))) {
+    for (let i in s1) {
+        if (map.has(s1.charAt(i))) {
             map.set(s1.charAt(i), map.get(s1.charAt(i)) + 1)
         } else {
             map.set(s1.charAt(i), 1)
         }
     }
-    for(let i in s2) {
-        if(map.has(s2.charAt(i))) {
+    for (let i in s2) {
+        if (map.has(s2.charAt(i))) {
             map.set(s2.charAt(i), map.get(s2.charAt(i)) - 1)
-            if(map.get(s2.charAt(i)) < 0) return false
+            if (map.get(s2.charAt(i)) < 0) return false
         } else {
             return false
         }
     }
-    return true   
+    return true
 }
 
 //Approach 2: Using string.sort and compare each
 //character O(1) space, O(nlog(n)) time
 
-isPermutation = function(s1, s2) {
-    if(s1.length != s2.length) return false
-    s1 = s1.split('').sort().toString()
-    s2 = s2.split('').sort().toString()
-    if(s1 === s2) return true
+isPermutation = function (s1, s2) {
+    if (s1.length != s2.length) return false
+    s1 = s1.split("").sort().toString()
+    s2 = s2.split("").sort().toString()
+    if (s1 === s2) return true
     return false
 }
 
 /*console.log(isPermutation('aba', 'bac'))
 console.log(isPermutation('aab', 'aaa'))
 console.log(isPermutation('cars', 'racs'))*/
-
 
 /*1.4 Palindrome Permutation
 Given a string, write a function to check if it is a 
@@ -93,25 +90,27 @@ Output: True (permutations: "taco cat", "atco eta", etc.)
 */
 
 //Approach 1: Use Map() to store count of characters
-//and check if palindrome O(n) space + time 
+//and check if palindrome O(n) space + time
 
-var palindromePermutation = function(string) {
+var palindromePermutation = function (string) {
     var map = new Map()
-    for(let i in string) {
-        if(map.has(string.charAt(i))) {
+    for (let i in string) {
+        if (map.has(string.charAt(i))) {
             map.set(string.charAt(i), map.get(string.charAt(i)) + 1)
         } else {
             map.set(string.charAt(i), 1)
         }
     }
     var odd = true
-    if(string.length % 2 === 0) {
+    if (string.length % 2 === 0) {
         odd = false //string length is even
     }
     console.log(map)
-    for(let key of map.keys()) { //must use 'for' 'of' with map iterables
-        if(map.get(key) % 2 != 0) { //not even
-            if(!odd) {
+    for (let key of map.keys()) {
+        //must use 'for' 'of' with map iterables
+        if (map.get(key) % 2 != 0) {
+            //not even
+            if (!odd) {
                 return false
             } else {
                 odd = false
@@ -123,7 +122,6 @@ var palindromePermutation = function(string) {
 
 /*console.log(palindromePermutation('tacocat'))
 console.log(palindromePermutation('ca'))*/
-
 
 /* 1.5 One Away
 There are three types of edits that can be performed
@@ -138,24 +136,25 @@ pale, bale -> true
 pale, bake -> false
 */
 
-var oneAway = function(s1, s2) {
-    if(Math.abs(s1.length - s2.length) > 1) return false
+var oneAway = function (s1, s2) {
+    if (Math.abs(s1.length - s2.length) > 1) return false
     var found = false
     //Case 1: Replacing character
-    if(s1.length === s2.length) {
-        for(let i in s1) {
-            if(s1.charAt(i) !== s2.charAt(i)) {
-                if(found) return false
+    if (s1.length === s2.length) {
+        for (let i in s1) {
+            if (s1.charAt(i) !== s2.charAt(i)) {
+                if (found) return false
                 found = true
             }
         }
-    } else { //Case 2: Insert/Removing
+    } else {
+        //Case 2: Insert/Removing
         var larger = s1.length > s2.length ? s1 : s2
         var smaller = larger === s1 ? s2 : s1
-        var j = 0;
-        for(let i in smaller) {
-            if(smaller.charAt(i) !== larger.charAt(j)) {
-                if(found) return false
+        var j = 0
+        for (let i in smaller) {
+            if (smaller.charAt(i) !== larger.charAt(j)) {
+                if (found) return false
                 found = true
                 j = j + 1
             }
@@ -170,7 +169,6 @@ console.log(oneAway('pales', 'pale'))
 console.log(oneAway('pale', 'bale'))
 console.log(oneAway('pale', 'bake'))*/
 
-
 /* 1.6 String Compression
 Implement a method to perform basic string 
 compression using the counts of repeated characters. 
@@ -184,16 +182,18 @@ the string has only uppercase and lowercase letters
 
 //Approach 1: Use another string to capture
 //result string and compare until char changes
-var stringCompression = function(string) {
-    if(string.length <= 2) return string
-    var res = ''
+var stringCompression = function (string) {
+    if (string.length <= 2) return string
+    var res = ""
     let count = 0
-    var i = 0, j = 0;
-    while(i < string.length) {
-        if(string.charAt(i) === string.charAt(j)) {
+    var i = 0,
+        j = 0
+    while (i < string.length) {
+        if (string.charAt(i) === string.charAt(j)) {
             i++
             count++
-        } else { //switch 
+        } else {
+            //switch
             res += string.charAt(j) + count
             count = 0
             j = i
@@ -202,7 +202,6 @@ var stringCompression = function(string) {
     res += string.charAt(j) + count
     return string.length > res.length ? res : string
 }
-
 
 /*console.log(stringCompression('aabccccaaa'))
 console.log(stringCompression('aAbccCCaaa'))*/
@@ -213,10 +212,9 @@ an MxN matrix is 0, its entire row and column
 are set to 0.
 */
 
-
 //Approach 1: Using boolean matrix to mark 0s
 //and then zero out in original matrix
-var zeroMatrix = function(matrix) {
+var zeroMatrix = function (matrix) {
     let matrix2 = []
     matrix.forEach((element, i) => {
         matrix2[i] = []
@@ -226,30 +224,32 @@ var zeroMatrix = function(matrix) {
     })
     matrix.forEach((element, i) => {
         element.forEach((item, j) => {
-            if(matrix[i][j] === 0) {
+            if (matrix[i][j] === 0) {
                 helper(matrix2, i, j)
             }
         })
     })
-    for(let i = 0; i < matrix.length; i++) {
-        for(let j = 0; j < matrix[i].length; j++) {
-            if(matrix2[i][j] === false) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix2[i][j] === false) {
                 matrix[i][j] = 0
             }
         }
     }
     return matrix
 }
-var helper = function(matrix, i, j) {
-    for(let a = 0; a <= i; a++) {
+var helper = function (matrix, i, j) {
+    for (let a = 0; a <= i; a++) {
         matrix[a][j] = false
     }
-    for(let b = 0; b <= j; b++) {
+    for (let b = 0; b <= j; b++) {
         matrix[i][b] = false
     }
 }
 
-var matrix1 = [[1,2,3,4],[5,6,7,8],[9,0,11,12]]
+var matrix1 = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, 11, 12],
+]
 console.log(zeroMatrix(matrix1))
-
-
--- chapter2.js
+++ chapter2.js
@@ -3,34 +3,33 @@
 //Declaration for Linked List using JS.
 //Each node, must have a pointer and value
 var node = function (x) {
-  this.data = x;
-  this.next = null;
-};
+    this.data = x
+    this.next = null
+}
 
 //Add node to end of linked list
 //Only usable when setting up test cases
 function addNodeToTail(head, x) {
-  var n = new node(x);
-  var curr = head;
-  if (curr === null || curr === undefined) {
-    head = n;
-  } else {
-    while (curr.next != null) {
-      curr = curr.next;
+    var n = new node(x)
+    var curr = head
+    if (curr === null || curr === undefined) {
+        head = n
+    } else {
+        while (curr.next != null) {
+            curr = curr.next
+        }
     }
-  }
-  curr.next = n;
+    curr.next = n
 }
 
 //Add node to head of linked list
 //Only usable when setting up test cases
 function addNodeToHead(head, x) {
-  var n = new node(x);
-  n.next = head;
-  head = n;
+    var n = new node(x)
+    n.next = head
+    head = n
+    return head
 }
-
-
 
 /* 2.1 Remove Dups
 Write code to remove duplicates from an unsorted 
@@ -40,4 +39,178 @@ How would you solve this problem if a temporary
 buffer is not allowed?
 */
 
-//Approach 1:
+//Approach 1: Using Map(), store values and then
+//delete duplicate nodes found
+//O(n) time + space
+var removeDup = function (head) {
+    var map = new Map()
+    var curr = head
+    var prev = null
+    if (head.next === null) {
+        return head
+    }
+    while (curr != null) {
+        if (!map.has(curr.data)) {
+            map.set(curr.data, 1)
+            prev = curr
+        } else {
+            //found duplication, remove
+            prev.next = curr.next
+        }
+        curr = curr.next
+    }
+    return head
+}
+
+//Approach 2: Use 2 pointers to iterate through linked list
+//O(n^2) time, O(1) space
+removeDup = function (head) {
+    var curr = head
+    var pt1 = head
+    var prev = head
+    if (head.next === null) {
+        return head
+    }
+    while (curr.next != null) {
+        //first loop
+        pt1 = curr.next
+        prev = curr
+        while (pt1 != null) {
+            if (pt1.data === curr.data) {
+                prev.next = pt1.next
+            } else {
+                prev = pt1
+            }
+            pt1 = pt1.next
+        }
+        curr = curr.next
+    }
+    return head
+}
+
+//Test Cases
+/*var head = new node(1)
+addNodeToTail(head, 2)
+addNodeToTail(head, 2)
+addNodeToTail(head, 3)
+addNodeToTail(head, 4)
+addNodeToTail(head, 5)
+head = addNodeToHead(head, 1)
+head = addNodeToHead(head, 1)
+head = addNodeToHead(head, 1)
+
+var curr = head
+while (curr != null) {
+  console.log(curr)
+  curr = curr.next
+}
+var res = removeDup(head)
+curr = res
+while (curr != null) {
+  console.log(curr)
+  curr = curr.next
+}*/
+
+/* 2.2 Return Kth to Last
+Implement an algorithm to find the kth to last 
+element of a singly linked list.
+*/
+
+//Approach 1: Use a stack and pop k times
+//to get kth to last element
+//O(n) time + space
+kthToLast = function (head, k) {
+    var stack = [] //using array as stack
+    while (head != null) {
+        stack.push(head)
+        head = head.next
+    }
+    for (let i = 0; i < k; i++) {
+        var res = stack.pop()
+    }
+    return res
+}
+
+//Approach 2: Use recursion to push and pop
+//k times
+//This approach uses index for recursing
+//O(n) time, O(1) space
+function kthToLastRecursion(head, k) {
+    if (head === null) return 0
+    var ind = kthToLastRecursion(head.next, k) + 1
+    if (ind === k) console.log(head)
+    return ind
+}
+
+//Aproach 3: Iterate with 2 pointers,
+//distance them by k nodes and go till end of list
+//O(n) time, O(1) space
+kthToLastIterative = function (head, k) {
+    var pt1 = head
+    var pt2 = head
+    for (let i = 0; i < k; i++) {
+        pt2 = pt2.next
+    }
+    while (pt2 != null) {
+        pt1 = pt1.next
+        pt2 = pt2.next
+    }
+    return pt1
+}
+
+/*var head = new node(1)
+addNodeToTail(head, 2)
+addNodeToTail(head, 3)
+addNodeToTail(head, 4)
+addNodeToTail(head, 5)
+addNodeToTail(head, 6)
+
+var res = kthToLastRecursion(head, 5)
+res = kthToLast(head, 5)
+console.log(res)
+res = kthToLastIterative(head, 5)
+console.log(res)*/
+
+
+/* 2.3 Delete Middle Node
+Implement an algorithm to delete a node in the 
+middle (i.e., any node but the first and last node, 
+not necessarily the exact middle) of a singly linked 
+list, given only access to that node.
+
+EXAMPLE
+lnput:the node c from the l
+inked list a->b->c->d->e->f
+
+Result: nothing is returned, but the new l
+inked list looks like a->b->d->e->f
+*/
+
+//Approach 1
+//Change data of curr and keep going till final node,
+//then set it to null
+//O(n) time, O(1) space
+
+var deleteMiddleNode = function(curr) {
+    while(curr.next != null) {
+        curr.data = curr.next.data
+        curr = curr.next
+    }
+}
+
+var head = new node(0)
+addNodeToTail(head, 1)
+addNodeToTail(head, 2)
+addNodeToTail(head, 3)
+addNodeToTail(head, 4)
+addNodeToTail(head, 5)
+
+var curr = head
+for(let i = 0; i < 3; i++) {
+    curr = curr.next
+}
+deleteMiddleNode(curr)
+while(head != null) {
+    console.log(head)
+    head = head.next
+}
